605600898efd8530ad46f992127813647c1f7d1b
Lando Silva <orlandobsilva@outlook.com>
2025-07-18 23:12:09 +0200

    Switch to EditorPrefs-based settings

--- Assets/Transitions/Runtime/TransitionsSettings.cs
+++ Assets/Transitions/Runtime/TransitionsSettings.cs
@@ -1,6 +1,3 @@
-using System;
-using System.IO;
-using System.Linq;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -8,66 +5,49 @@ using UnityEditor;
 
 namespace Lando.Transitions
 {
-    public class TransitionsSettings : ScriptableObject
+    public static class TransitionsSettings
     {
-        [field: SerializeField] public TransitionViewMonoBehaviour DefaultTransitionPrefab { get; private set; }
-        [SerializeField] private ScenePathClassGenerationSettings _scenePathClassGenerationSettings;
+#if UNITY_EDITOR
+        private const string PREF_DEFAULT_TRANSITION_GUID = "Transitions.DefaultTransitionGuid";
+        private const string PREF_NAMESPACE = "Transitions.ScenePathNamespace";
+        private const string PREF_PATH = "Transitions.ScenePathPath";
+        private const string PREF_CLASSNAME = "Transitions.ScenePathClassName";
+#endif
 
-        [NonSerialized] private static ITransitionView _defaultTransition;
-        public static ScenePathClassGenerationSettings ScenePathClassGenerationSettings 
-            => Instance._scenePathClassGenerationSettings;
-        
-        private static TransitionsSettings _instance;
-        private static TransitionsSettings Instance
+        private static ITransitionView _defaultTransition;
+
+        public static ScenePathClassGenerationSettings ScenePathClassGenerationSettings
         {
             get
             {
 #if UNITY_EDITOR
-                _instance = LoadOrCreateSettings();
+                return new ScenePathClassGenerationSettings
+                {
+                    Namespace = EditorPrefs.GetString(PREF_NAMESPACE, "Generated"),
+                    Path = EditorPrefs.GetString(PREF_PATH, "Assets/Scripts/Generated/"),
+                    ClassName = EditorPrefs.GetString(PREF_CLASSNAME, "ScenePath")
+                };
 #else
-                string scriptName = nameof(TransitionsSettings);
-                _instance = Resources.Load<TransitionsSettings>(scriptName);
+                return new ScenePathClassGenerationSettings();
 #endif
-                return _instance;
             }
         }
-        
+
         public static ITransitionView GetDefaultTransitionView() => _defaultTransition ??= CreateDefaultTransition();
+
         private static ITransitionView CreateDefaultTransition()
         {
-            return Instance.DefaultTransitionPrefab == null
-                ? new LazyBlackFadeInFadeOut()
-                : Instantiate(Instance.DefaultTransitionPrefab);
-        }
-
 #if UNITY_EDITOR
-        [InitializeOnLoadMethod]
-        private static void EnsureSettingsExist() => LoadOrCreateSettings();
-        
-        private static TransitionsSettings LoadOrCreateSettings()
-        {
-            const string scriptName = nameof(TransitionsSettings);
-            _instance ??= Resources.Load<TransitionsSettings>(scriptName) ?? CreateSettings();
-            return _instance;
-        }
-        
-        private static TransitionsSettings CreateSettings()
-        {
-            const string scriptName = nameof(TransitionsSettings);
-            string scriptableObjectName = $"{scriptName}.asset";
-            
-            string filter = $"t:MonoScript {scriptName}";
-            string scriptPath = AssetDatabase.FindAssets(filter).Select(AssetDatabase.GUIDToAssetPath).ElementAt(0);
-            string directory = Path.GetDirectoryName(scriptPath) ?? string.Empty;
-            string path = Path.Combine(directory, "../Resources", scriptableObjectName);
-            
-            TransitionsSettings settings = CreateInstance<TransitionsSettings>();
-            AssetDatabase.CreateAsset(settings, path);
-            AssetDatabase.SaveAssets();
-
-            Debug.Log("Transition Settings Created at: " + path);
-            return settings;
-        }
+            string guid = EditorPrefs.GetString(PREF_DEFAULT_TRANSITION_GUID, string.Empty);
+            if (!string.IsNullOrEmpty(guid))
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                var prefab = AssetDatabase.LoadAssetAtPath<TransitionViewMonoBehaviour>(path);
+                if (prefab != null)
+                    return Object.Instantiate(prefab);
+            }
 #endif
+            return new LazyBlackFadeInFadeOut();
+        }
     }
 }
